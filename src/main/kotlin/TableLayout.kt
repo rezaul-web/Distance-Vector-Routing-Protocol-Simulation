8b1ffc6b8f4736271ab0820d6f8e540cb80199f8
rezaul-web <rmdrezaul75@gmail.com>
2025-04-15 12:09:38 +0530

    Enhanced table

--- src/main/kotlin/TableLayout.kt
+++ src/main/kotlin/TableLayout.kt
@@ -42,6 +42,31 @@ fun TableLayout(
         shape = RoundedCornerShape(12.dp),
         backgroundColor = TableColors.cardBackground
     ) {
+        val s = when(timer) {
+            0->"Zero"
+            1 -> "First"
+            2 -> "Second"
+            3 -> "Third"
+            4 -> "Fourth"
+            5 -> "Fifth"
+            6 -> "Sixth"
+            7 -> "Seventh"
+            8 -> "Eighth"
+            9 -> "Ninth"
+            10 -> "Tenth"
+            11 -> "Eleventh"
+            12 -> "Twelfth"
+            13 -> "Thirteenth"
+            14 -> "Fourteenth"
+            15 -> "Fifteenth"
+            16 -> "Sixteenth"
+            17 -> "Seventeenth"
+            18 -> "Eighteenth"
+            19 -> "Nineteenth"
+            20 -> "Twentieth"
+            else -> "kdfgk"
+        }
+
         Column(
             modifier = Modifier
                 .padding(horizontal = 16.dp, vertical = 12.dp)
@@ -51,7 +76,7 @@ fun TableLayout(
             // --- Title (Fixed at top) ---
             Text(
                 // text = "Router $selectedNode Table (Step: $timer)", // Use step/timer
-                text = "Router $selectedNode Table (T: $timer)",
+                text = "Router $selectedNode Table (After $s Exchange)",
                 fontWeight = FontWeight.ExtraBold,
                 fontSize = 18.sp,
                 color = TableColors.primaryText,
